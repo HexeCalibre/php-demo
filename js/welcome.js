bc9ce800f78f83693929ed3cef9877e74865af71
IamIronMan2023 <kramsotnas@yahoo.com>
2023-07-29 08:46:23 +0800

    remove demo codes

--- js/welcome.js
+++
@@ -1,7 +0,0 @@
-function deactivate(loginName) {
-  $.post("deactivate.php", { login_name: loginName }).done(function (data) {
-    window.location = "http://localhost/php-demo/login.php";
-  });
-
-  return false;
-}
