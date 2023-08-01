43057ed18d992ee47dd88ae709432da46e55dfe4
IamIronMan2023 <kramsotnas@yahoo.com>
2023-08-01 21:09:55 +0800

    Add transaction lesson

--- mysqli/mysqli.php
+++ mysqli/mysqli.php
@@ -20,6 +20,23 @@ while ($row = $users->fetch_assoc()) {
     echo $row['first_name'] . " " . $row['last_name'] . "<br>";
 }
 
+echo nl2br("\n\n===================SELECT Statement Parameterize=======\n");
+
+$sql = "SELECT * FROM user WHERE id = ?";
+
+$id = 2;
+
+$stmt = $con->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+}
+
+echo $user['first_name'] . " " . $user['last_name'] . "<br>";
+
+
 $first_name = "Juan";
 $last_name = "Dela Cruz";
 $user_name = "jdelacruz";
@@ -34,4 +51,26 @@ $stmt->bind_param("sssss", $first_name, $last_name, $user_name, $password, $emai
 
 if ($stmt->execute()) {
     echo "Insert successfull";
+}
+
+echo nl2br("\n\n===================UPDATE Statement=======\n");
+$sql = "UPDATE user SET first_name = ?, last_name = ?, user_name = ?, password = ?, email = ? WHERE id = ?";
+
+$id = 1;
+$stmt = $con->prepare($sql);
+$stmt->bind_param("sssssi", $first_name, $last_name, $user_name, $password, $email, $id);
+
+if ($stmt->execute()) {
+    echo "Update successfull";
+}
+
+echo nl2br("\n\n===================DELETE Statement=======\n");
+$sql = "DELETE FROM user WHERE id = ?";
+
+$id = 1;
+$stmt = $con->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    echo "Delete successfull";
 }
